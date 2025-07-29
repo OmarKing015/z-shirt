901c02e7e0708f6eea4a9ba7ba05763801d832a3
fantcy012az <fantcy012az@gmail.com>
2025-07-29 04:17:53 +0000

    cost summarry resposive

--- components/customizer/CostSummay.tsx
+++ components/customizer/CostSummay.tsx
@@ -13,18 +13,19 @@ export default function CostSummary() {
   }, [totalCost])
 
   return (
-    <Card className="fixed bottom-4 right-4 w-64 shadow-2xl backdrop-blur-sm bg-card/80">
-      <CardHeader>
-        <CardTitle className="text-lg">Total Cost</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <p className="text-3xl font-bold text-black">
-          {totalCost.toFixed(2)} EGP
-        </p>
-        <p className="text-xs text-muted-foreground mt-1">
-          Real-time price calculation.
-        </p>
-      </CardContent>
-    </Card>
+    <Card className="w-full max-w-xs sm:mx-auto sm:mt-8 m-5 lg:fixed lg:bottom-4 lg:right-4 lg:w-64 shadow-2xl backdrop-blur-sm bg-card/80">
+  <CardHeader>
+    <CardTitle className="text-lg">Total Cost</CardTitle>
+  </CardHeader>
+  <CardContent>
+    <p className="text-3xl font-bold text-black">
+      {totalCost.toFixed(2)} EGP
+    </p>
+    <p className="text-xs text-muted-foreground mt-1">
+      Real-time price calculation.
+    </p>
+  </CardContent>
+</Card>
+
   );
 }
